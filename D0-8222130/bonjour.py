bc17270bbad9f56b760fb894ed5f5eefc1eb0545
VPoint <vpoint000@gmail.com>
2015-11-07 17:30:21 -0500

    Homework One
    This is the first testing the submission process.

---
+++ D0-8222130/bonjour.py
@@ -0,0 +1,5 @@
+# Program 0 - ITI 1520Z
+# Numero d'étudiante: 8222130
+# Nom: Esther Raji
+
+print("Bonjour")
---
+++ D0-8222130/bonjour1.py
@@ -0,0 +1,5 @@
+# Program 1 - ITI 1520Z
+# Numero d'étudiante: 8222130
+# Nom: Esther Raji
+
+print("Bonjour Esther.")
